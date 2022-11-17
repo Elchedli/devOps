6fb68ebd304421113710596f787cbfebc3afb6d4
zroussama <oussama.zribi@esprit.tn>
2022-11-17 16:33:08 +0100

    2.9 Long

--- src/test/java/com/esprit/examen/services/SecteurActiviteServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/SecteurActiviteServiceImplTest.java
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+
 @ExtendWith(MockitoExtension.class)
 public class SecteurActiviteServiceImplTest {
 
